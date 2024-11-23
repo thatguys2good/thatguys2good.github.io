eb98a2256df330faceede4699c59faa5c49e2a2a
thatguys2good <da0ro1jecu@hotmail.com>
2024-11-23 07:27:50 -0500

    Add files via upload

---
+++ bootstrapper.js
@@ -0,0 +1,23 @@
+// Loads the game scripts all at once
+(async function() {
+    const scripts = [
+        // "scsocket.js",
+        "vars.js",
+        "render.js",
+        "sha256.js",
+        "game.js",
+        "handlers.js",
+        "events.js",
+        "settings.js",
+        "leaderboard.js"
+    ]
+    let fullSource = "";
+    for (let s in scripts) {
+        await fetch(scripts[s]).then(function(response) {
+          return response.text();
+        }).then(function(text) {
+          fullSource += text;
+        });;
+    }
+    eval("(function() {" + fullSource + "})();");
+})();
